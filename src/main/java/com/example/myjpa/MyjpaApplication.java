94e0a458a368a82a8ffd72efe184f375a35c9163
rex <rex@sentbe.com>
2022-03-03 09:03:40 +0900

    4th commit

--- src/main/java/com/example/myjpa/MyjpaApplication.java
+++ src/main/java/com/example/myjpa/MyjpaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyjpaApplication {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("hihohoho");
         SpringApplication.run(MyjpaApplication.class, args);
     }
 
